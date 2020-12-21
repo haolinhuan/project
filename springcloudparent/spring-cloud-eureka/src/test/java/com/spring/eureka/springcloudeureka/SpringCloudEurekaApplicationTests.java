05fb22adbeaf8c82c1a75a70c1ee0bffcd03f1a2
haolinhuan <2511846341@qq.com>
2020-12-21 22:34:18 +0800

    spring cloud

---
+++ springcloudparent/spring-cloud-eureka/src/test/java/com/spring/eureka/springcloudeureka/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.spring.eureka.springcloudeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
